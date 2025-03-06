50884adeebe1dfb4415df2cbf5e4a77be9510610
bhuwan <bkspoudel@gmail.com>
2025-03-06 14:39:17 +0100

    create a header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContainerComponent],
+  imports: [RouterOutlet, ContainerComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
