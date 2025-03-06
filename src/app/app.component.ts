5eabc947d08788e3f07db8733948df262812f871
bhuwan <bkspoudel@gmail.com>
2025-03-06 14:37:13 +0100

    create a container component to wrap others

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
