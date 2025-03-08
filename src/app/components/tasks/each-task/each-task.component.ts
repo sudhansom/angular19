1dcb7f1bc27927bd360c6c1c774a148634b508b3
bhuwan <bkspoudel@gmail.com>
2025-03-08 21:09:14 +0100

    add card component and style

--- src/app/components/tasks/each-task/each-task.component.ts
+++ src/app/components/tasks/each-task/each-task.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input } from '@angular/core';
 import { ContainerComponent } from '../../container/container.component';
+import { CardComponent } from '../../shared/card/card.component';
 import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-each-task',
-  imports: [ContainerComponent, DatePipe],
+  imports: [ContainerComponent, DatePipe, CardComponent],
   templateUrl: './each-task.component.html',
   styleUrl: './each-task.component.scss'
 })
