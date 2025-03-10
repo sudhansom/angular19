4324ef43b9b1a13edce5b4b67b63e7a3fffbb195
bhuwan <bkspoudel@gmail.com>
2025-03-10 14:33:19 +0100

    set addTask function

--- src/app/components/tasks/new-task/new-task.component.ts
+++ src/app/components/tasks/new-task/new-task.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CardComponent } from '../../shared/card/card.component';
 import { ContainerComponent } from '../../container/container.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [ContainerComponent, CardComponent],
+  imports: [ContainerComponent, CardComponent, FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
+id = '';
+title = '';
+desc = '';
+user = '';
 
+addTask(){
+  console.log(this.id, this.title, this.desc, this.user);
+}
 }
