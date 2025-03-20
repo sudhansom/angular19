6f2fcda9771b6222fa0160a06401a0087be822fc
bhuwan <bkspoudel@gmail.com>
2025-03-20 09:27:20 +0100

    create a view page for user registration form

---
+++ src/app/views/user-registration/user-registration.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-registration',
+  imports: [],
+  templateUrl: './user-registration.component.html',
+  styleUrl: './user-registration.component.scss'
+})
+export class UserRegistrationComponent {
+
+}
