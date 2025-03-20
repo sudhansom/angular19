3b640aa5ec757af2da529a4aee5f299c0a9a20d8
bhuwan <bkspoudel@gmail.com>
2025-03-20 14:54:06 +0100

    create form with all possible inputs

--- src/app/views/user-registration/user-registration.component.ts
+++ src/app/views/user-registration/user-registration.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import { FormsModule, NgForm } from '@angular/forms';
 @Component({
   selector: 'app-user-registration',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, FormsModule],
   templateUrl: './user-registration.component.html',
   styleUrl: './user-registration.component.scss'
 })
 export class UserRegistrationComponent {
-
+formSubmit(form: NgForm){
+  console.log(form);
+}
 }
