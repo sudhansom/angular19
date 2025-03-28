31503242080c44d438ad7ddca5f2327a9621d4e7
bhuwan <bkspoudel@gmail.com>
2025-03-28 11:32:28 +0100

    add a reactive form in ts file

--- src/app/views/reactive-registration/reactive-registration.component.ts
+++ src/app/views/reactive-registration/reactive-registration.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-registration',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './reactive-registration.component.html',
   styleUrl: './reactive-registration.component.scss'
 })
 export class ReactiveRegistrationComponent {
+reactiveForm = new FormGroup({
+  firstName: new FormControl('', Validators.required),
+})
 
+submitForm(){
+  console.log(this.reactiveForm);
+}
 }
