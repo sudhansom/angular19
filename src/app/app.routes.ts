21e543bb3247d2da4ef7a71dae246a3e93a31c41
bhuwan <bkspoudel@gmail.com>
2025-03-28 11:23:39 +0100

    add a route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { TasksManageComponent } from './views/tasks-manage/tasks-manage.component';
 import { UserRegistrationComponent } from './views/user-registration/user-registration.component';
+import { ReactiveRegistrationComponent } from './views/reactive-registration/reactive-registration.component';
 
 export const routes: Routes = [
   {path: '', component: TasksManageComponent},
-  {path: 'register', component: UserRegistrationComponent}
+  {path: 't-register', component: UserRegistrationComponent},
+  {path: 'r-register', component: ReactiveRegistrationComponent}
 ];
