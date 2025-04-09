9eb3026371292a27f35d06df800e693514f4f661
bhuwan <bkspoudel@gmail.com>
2025-04-09 14:37:26 +0200

    show the error message if the field is touched and invalid

--- src/app/views/reactive-registration/reactive-registration.component.ts
+++ src/app/views/reactive-registration/reactive-registration.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators, FormsModule, FormArray } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reactive-registration',
-  imports: [ContainerComponent, FormsModule, ReactiveFormsModule],
+  imports: [ContainerComponent, CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './reactive-registration.component.html',
   styleUrl: './reactive-registration.component.scss'
 })
