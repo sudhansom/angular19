d7918d27522c052acce6d1d8843a494deb2ffafa
bhuwan <bkspoudel@gmail.com>
2025-04-15 16:48:05 +0200

    remove un-necessary component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TasksManageComponent } from './views/tasks-manage/tasks-manage.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, TasksManageComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
