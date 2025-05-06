5a69da4b8f8c3ade8d917139ae567b272089adf2
bhuwan <bkspoudel@gmail.com>
2025-05-06 15:11:37 +0200

    delete all and start over

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,6 @@
 import { Routes } from '@angular/router';
-import { TasksManageComponent } from './views/tasks-manage/tasks-manage.component';
-import { UserRegistrationComponent } from './views/user-registration/user-registration.component';
-import { ReactiveRegistrationComponent } from './views/reactive-registration/reactive-registration.component';
-import { BudgetComponent } from './views/budget/budget.component';
+
 
 export const routes: Routes = [
-  {path: '', component: TasksManageComponent},
-  {path: 't-register', component: UserRegistrationComponent},
-  {path: 'r-register', component: ReactiveRegistrationComponent},
-  {path: 'budget', component: BudgetComponent},
 
 ];
--- src/app/components/tasks/tasks.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Task {
-  id: string;
-  title: string;
-  description: string;
-  date: string;
-  user: string;
-}
