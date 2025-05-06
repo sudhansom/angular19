cb2dd3bbb818910474a257579ca9a79c44e35074
bhuwan <bkspoudel@gmail.com>
2025-05-06 14:13:15 +0200

    create a function in budget service to calculate the interests

--- src/app/services/budget.service.ts
+++ src/app/services/budget.service.ts
@@ -8,4 +8,25 @@ export class BudgetService {
   results = signal<string>('');
 
   constructor() { }
+
+
+  calculateResult(duration: number, initialInvestment: number, annualInvestment: number, expectedReturn: number){
+    let investmentValue = 0;
+    let annualData = [];
+    for( let i = 0; i < duration; i++){
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+  }
+
 }
