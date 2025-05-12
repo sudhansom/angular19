f4bf1b3829265ae8ee46aacfdef3f57ee4fa4327
bhuwan <bkspoudel@gmail.com>
2025-05-12 13:04:34 +0200

    add a directive safe-link

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { SafeLinkDirective } from '../../directives/safe-link.directives';
+
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, SafeLinkDirective],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
