41d3379e210cc6274dae670b278b878aa2325c26
bhuwan <bkspoudel@gmail.com>
2025-05-14 22:29:30 +0200

    clear all to proctice rxjs stream

--- src/app/views/rxjs/practice/practice.component.ts
+++ src/app/views/rxjs/practice/practice.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 
 import { ContainerComponent } from '../../../components/container/container.component';
-import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-practice',
@@ -11,17 +10,5 @@ import { BehaviorSubject } from 'rxjs';
   styleUrl: './practice.component.scss'
 })
 export class PracticeComponent {
-  counter = new BehaviorSubject<number>(0);
 
-  counter$ = this.counter.asObservable();
-
-  onIncrement(){
-    // this.counter.update(preValue => preValue + 1);
-    this.counter.next(this.counter.value + 1);
-  }
-
-  onDecrement(){
-    // this.counter.update(preValue => preValue - 1);
-    this.counter.next(this.counter.value - 1);
-  }
 }
