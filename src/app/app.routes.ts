9b63c2d760648b12305b487ce58b038b3b0290e9
bhuwan <bkspoudel@gmail.com>
2025-05-14 21:13:16 +0200

    create a separate component to practice rxjs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { UserRegistrationComponent } from './views/user-registration/user-regist
 import { ReactiveRegistrationComponent } from './views/reactive-registration/reactive-registration.component';
 import { BudgetComponent } from './views/budget/budget.component';
 import { SeparationComponent } from './views/separation/separation.component';
+import { PracticeComponent } from './views/rxjs/practice/practice.component';
 
 export const routes: Routes = [
   {path: '', component: TasksManageComponent},
@@ -11,5 +12,6 @@ export const routes: Routes = [
   {path: 'r-register', component: ReactiveRegistrationComponent},
   {path: 'budget', component: BudgetComponent},
   { path: 'separation', component: SeparationComponent },
+  { path: 'rxjs', component: PracticeComponent},
 
 ];
