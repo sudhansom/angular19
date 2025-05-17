6b3e46fb07f1f1e0ebf71554cc9728b95be63107
bhuwan <bkspoudel@gmail.com>
2025-05-17 22:39:30 +0200

    filter beginner from the courses and display in template uning async pipe

--- src/app/views/rxjs/practice/practice.component.ts
+++ src/app/views/rxjs/practice/practice.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 
 import { ContainerComponent } from '../../../components/container/container.component';
-import { Observable, fromEvent, timer,map } from 'rxjs';
+import { Observable, filter, timer,map } from 'rxjs';
+
+type Course = {
+  id: number,
+  title: string,
+  level: string
+}
 
 @Component({
   selector: 'app-practice',
@@ -11,26 +17,33 @@ import { Observable, fromEvent, timer,map } from 'rxjs';
   styleUrl: './practice.component.scss'
 })
 export class PracticeComponent implements OnInit {
+  beginner$ = Observable<Course[]>
+  advanced$ = Observable<Course[]>
   ngOnInit() {
     const http$ = createHttpObservable('http://localhost:3000/courses');
 
     const data$ = http$.pipe(map(d => d["payload"]));
 
-    data$.subscribe(data => {
-      console.log(data);
-    },
-    (error)=>{console.log(error);
-    },
-    ()=>{console.log('completed')}
-    )
+
+    this. beginner$ = data$.filter(c => c.level === 'beginner');
+
+    // this.beginner$.subscribe(data => {
+    //   console.log(data);
+    // },
+    // (error)=>{console.log(error);
+    // },
+    // ()=>{console.log('completed')}
+    // )
 
     function createHttpObservable(url: string){
       return Observable.create(observer => {
         fetch(url).then(response => {
           return response.json();
         }).then(data => {
-          observer.next(data);
-          observer.complete();
+          setTimeout(() => {
+            observer.next(data);
+            observer.complete();
+          }, 1000);
         }).catch(err => {
           observer.error(err);
         })
