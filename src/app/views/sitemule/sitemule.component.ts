74359e40b9a942607ba95ad73c5f6217514c1fdf
bhuwan <bkspoudel@gmail.com>
2025-05-22 21:52:07 +0200

    add dummy data and style the container a bit

--- src/app/views/sitemule/sitemule.component.ts
+++ src/app/views/sitemule/sitemule.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+
+interface Features {
+  title: string,
+  description: string,
+}
 
 @Component({
   selector: 'app-sitemule',
@@ -7,6 +12,67 @@ import { ContainerComponent } from '../../components/container/container.compone
   templateUrl: './sitemule.component.html',
   styleUrl: './sitemule.component.scss'
 })
-export class SitemuleComponent {
+export class SitemuleComponent implements OnInit {
+  mylists = signal<Features[]>([]);
 
+  ngOnInit() {
+      this.mylists.set([
+        {
+          title: 'Community Edition1',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition2',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition3',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition4',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition5',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition6',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition7',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition8',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition9',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition10',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition11',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition12',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition13',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+        {
+          title: 'Community Edition14',
+          description: 'IceBreak Community is for developers who want to build and deploy Web applications using RPG and COBOL, and other technologies like SQL, XHTML, XML and AJAX.'
+        },
+      ])
+  }
 }
