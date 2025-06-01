638af12c80cc3b3b1b7b505a005c80f14b24746c
bhuwan <bkspoudel@gmail.com>
2025-06-01 09:20:54 +0200

    grid-template-area

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-container',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './container.component.scss'
 })
 export class ContainerComponent {
-
+@Input()width: string;
 }
