2e8ffe48813c6bc086db7ea224210880501d8d3b
bhuwan <bkspoudel@gmail.com>
2025-06-03 21:32:17 +0200

    create component for css after/before pseudoelement

---
+++ src/app/views/css-before/css-before.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-css-before',
+  imports: [],
+  templateUrl: './css-before.component.html',
+  styleUrl: './css-before.component.scss'
+})
+export class CssBeforeComponent {
+
+}
