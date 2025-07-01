8570207633b2de40fe40c42127633cc39b1dc134
bhuwan <bkspoudel@gmail.com>
2025-07-01 22:41:26 +0200

    show and hide child view

--- src/app/views/arrays/arrays.component.ts
+++ src/app/views/arrays/arrays.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
 import { NgIf } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-arrays',
-  imports: [ContainerComponent, NgIf],
+  imports: [ContainerComponent, NgIf, RouterModule],
   templateUrl: './arrays.component.html',
   styleUrl: './arrays.component.scss'
 })
