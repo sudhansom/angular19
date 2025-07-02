5cf243315fde032594dbc82ba71da9fb40e31dac
bhuwan <bkspoudel@gmail.com>
2025-07-02 13:03:50 +0200

    display the subscribed value

--- src/app/views/observables/observables.component.ts
+++ src/app/views/observables/observables.component.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { ContainerComponent } from '../../components/container/container.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-observables',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, AsyncPipe],
   templateUrl: './observables.component.html',
   styleUrl: './observables.component.scss'
 })
 export class ObservablesComponent {
+  name$ = new Subject();
   createObservable(){
     const observable$ = new Observable(observer => {
       observer.next('sudhan');
-      observer.next('Resham');
+      setTimeout(()=>{
+        observer.next('Resham')
+      }, 2000)
       setTimeout(()=>{
         observer.next('Biren')
-      }, 5000)
+      }, 4000)
     })
-    observable$.subscribe(console.log);
+    observable$.subscribe(value => {
+      this.name$.next(value);
+    });
   }
 }
