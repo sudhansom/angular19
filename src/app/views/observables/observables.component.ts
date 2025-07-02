af1e3a7fe36bc33b1801429b7f8f3c608cb53e64
bhuwan <bkspoudel@gmail.com>
2025-07-02 12:56:40 +0200

    create an observable and subscribe it

--- src/app/views/observables/observables.component.ts
+++ src/app/views/observables/observables.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContainerComponent } from '../../components/container/container.component';
 
 @Component({
   selector: 'app-observables',
-  imports: [],
+  imports: [ContainerComponent],
   templateUrl: './observables.component.html',
   styleUrl: './observables.component.scss'
 })
 export class ObservablesComponent {
-
+  createObservable(){
+    const observable$ = new Observable(observer => {
+      observer.next('sudhan');
+      observer.next('Resham');
+      setTimeout(()=>{
+        observer.next('Biren')
+      }, 5000)
+    })
+    observable$.subscribe(console.log);
+  }
 }
