50c9e0196049b069f4ceb53b08970fd891e62e6b
bhuwan <bkspoudel@gmail.com>
2025-08-05 17:04:55 +0200

    create a counter and implement the store to show the count value

---
+++ src/app/views/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { AsyncPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-counter',
+  imports: [AsyncPipe],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{counter: number}>){
+    this.count$ = store.select('counter');
+  }
+
+  decrement(){
+  }
+  increment(){
+
+  }
+}
--- src/app/views/ngrx/ngrx.component.ts
+++ src/app/views/ngrx/ngrx.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import { CounterComponent } from '../counter/counter.component';
 
 @Component({
   selector: 'app-ngrx',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, CounterComponent],
   templateUrl: './ngrx.component.html',
   styleUrl: './ngrx.component.scss'
 })
