ccef47046a1c43f4c2cbde9d53d45edc9caed6fb
bhuwan <bkspoudel@gmail.com>
2025-08-09 22:11:54 +0200

    create another selector from the existing one

--- src/app/store/app.selector.ts
+++ src/app/store/app.selector.ts
@@ -1,2 +1,7 @@
+import { createSelector, select } from "@ngrx/store";
 
 export const selectCount = ((state: {counter: number}) => state.counter);
+export const selectDoubleCount = createSelector(
+  selectCount,
+  (state) => state * 3
+)
