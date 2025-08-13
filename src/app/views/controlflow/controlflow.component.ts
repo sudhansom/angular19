259ebc27dca3c199d369646497cb74375c4a14fd
bhuwan <bkspoudel@gmail.com>
2025-08-13 22:50:18 +0200

    wrap with container

--- src/app/views/controlflow/controlflow.component.ts
+++ src/app/views/controlflow/controlflow.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '../../components/container/container.component';
 
 @Component({
   selector: 'app-controlflow',
-  imports: [],
+  imports: [ContainerComponent],
   templateUrl: './controlflow.component.html',
   styleUrl: './controlflow.component.scss'
 })
