10fcbca23e41793c9cca14c1c99416f5d0e39d0b
bhuwan <bkspoudel@gmail.com>
2025-08-15 11:54:39 +0200

    now we need to import NgIf to use it in template

--- src/app/views/controlflow/controlflow.component.ts
+++ src/app/views/controlflow/controlflow.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-controlflow',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, NgIf],
   templateUrl: './controlflow.component.html',
   styleUrl: './controlflow.component.scss'
 })
 export class ControlflowComponent {
   color = 'green'
   value = 1;
+  show = true;
 
   changeColor(color: string){
     this.color = color;
