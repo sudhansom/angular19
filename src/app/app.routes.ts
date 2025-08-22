55430abaa76f1fbf6ae95174656a83b8d2436a92
bhuwan <bkspoudel@gmail.com>
2025-08-22 21:31:42 +0200

    add one more lazyloading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { TasksManageComponent } from './views/tasks-manage/tasks-manage.component';
-import { UserRegistrationComponent } from './views/user-registration/user-registration.component';
 import { ReactiveRegistrationComponent } from './views/reactive-registration/reactive-registration.component';
 import { BudgetComponent } from './views/budget/budget.component';
 import { SeparationComponent } from './views/separation/separation.component';
@@ -15,12 +14,10 @@ import { ArraysComponent } from './views/arrays/arrays.component';
 import { ObservablesComponent } from './views/observables/observables.component';
 import { ObjectsComponent } from './views/objects/objects.component';
 import { PrototypeComponent } from './views/prototype/prototype.component';
-import { NgrxComponent } from './views/ngrx/ngrx.component';
-import { ControlflowComponent } from './views/controlflow/controlflow.component';
 
 export const routes: Routes = [
   {path: '', component: TasksManageComponent},
-  {path: 't-register', component: UserRegistrationComponent},
+  {path: 't-register', loadComponent: ()=>import('./views/user-registration/user-registration.component').then(c=>c.UserRegistrationComponent)},
   {path: 'r-register', component: ReactiveRegistrationComponent,
     children: [
       {
