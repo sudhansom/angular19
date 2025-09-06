bf709c64e19488554d599ac099498f635f511dd5
bhuwan <bkspoudel@gmail.com>
2025-09-06 14:10:43 +0200

    create a box and with few styles

--- src/app/views/css/css.component.ts
+++ src/app/views/css/css.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '../../components/container/container.component';
 
 @Component({
   selector: 'app-css',
-  imports: [],
+  imports: [ContainerComponent],
   templateUrl: './css.component.html',
   styleUrl: './css.component.scss'
 })
