29ccbba96966c97677c1a15e76fe5ce1be392e00
bhuwan <bkspoudel@gmail.com>
2025-09-09 20:38:34 +0200

    create a directive

---
+++ src/app/directives/show-me.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowMeDirective } from './show-me.directive';
+
+describe('ShowMeDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowMeDirective();
+    expect(directive).toBeTruthy();
+  });
+});
