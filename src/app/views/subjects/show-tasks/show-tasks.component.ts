053defeca7793acf0dad528493b45d33df6fb807
bhuwan <bkspoudel@gmail.com>
2025-09-23 14:40:26 +0200

    create new components to practice subjects

---
+++ src/app/views/subjects/show-tasks/show-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-tasks',
+  imports: [],
+  templateUrl: './show-tasks.component.html',
+  styleUrl: './show-tasks.component.scss'
+})
+export class ShowTasksComponent {
+
+}
