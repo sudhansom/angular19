728f193b9e5ad892e1c0e917e6d13089bc61016e
bhuwan <bkspoudel@gmail.com>
2025-10-14 20:43:33 +0200

    connect form to ngNorm in the template and send the form to the ts file through the function when the submit event is triggered

--- src/app/views/template/template.component.ts
+++ src/app/views/template/template.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, FormsModule],
   templateUrl: './template.component.html',
   styleUrl: './template.component.scss'
 })
 export class TemplateComponent {
+    formSubmit(form: NgForm){
+      console.log(form.value);
+      console.log(form.valid);
+
+    }
 
 }
