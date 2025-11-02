9024498a94748908cf0a4b84aff904d42073e80b
bhuwan <bkspoudel@gmail.com>
2025-11-02 22:39:10 +0100

    create a standard person class

--- src/app/components/prototype/js-file.ts
+++ src/app/components/prototype/js-file.ts
@@ -1,9 +1,20 @@
-export const Person = function(name, age, city){
-  this.name= name;
-  this.age = age;
-  this.city = city;
-}
+// export const Person = function(name, age, city){
+//   this.name= name;
+//   this.age = age;
+//   this.city = city;
+// }
 
-Person.prototype.showName = function(){
-  return `Name: ${this.name} and from City: ${this.city}`;
+// Person.prototype.showName = function(){
+//   return `Name: ${this.name} and from City: ${this.city}`;
+// }
+
+export class Person {
+  constructor(name, age, city){
+    this.name = name;
+    this.age = age;
+    this.city = city;
+  }
+  calcAge(){
+    return 'This function calculates the age.'
+  }
 }
