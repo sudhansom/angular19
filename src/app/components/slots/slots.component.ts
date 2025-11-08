f63382c0e95b89f323a715e5b523f4806d072920
Bhuwan Krishna Som Poudel <sudhansom@Bhuwans-MacBook-Pro.local>
2025-11-08 20:13:35 +0100

    create a component to practice slot

---
+++ src/app/components/slots/slots.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slots',
+  imports: [],
+  templateUrl: './slots.component.html',
+  styleUrl: './slots.component.scss'
+})
+export class SlotsComponent {
+
+}
