1b06f1177a40fb351ade1f00ed1ea1674b4ac431
Bhuwan Poudel <bkspoudel@gmail.com>
2025-11-11 07:25:41 +0100

    send ng-template to child-component from the parent

--- src/app/components/slots/slots.component.ts
+++ src/app/components/slots/slots.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef } from '@angular/core';
 import { ContainerComponent } from '../container/container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-slots',
-  imports: [ ContainerComponent ],
+  imports: [ ContainerComponent, CommonModule ],
   templateUrl: './slots.component.html',
   styleUrl: './slots.component.scss'
 })
 export class SlotsComponent {
+
+  showTemplate = false;
+
+  @Input() ngTempl: TemplateRef<any>;
 
 }
