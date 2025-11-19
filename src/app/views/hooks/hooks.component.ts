71908d9cdcc8b5bbef4f772e37475c20a12f31e0
Bhuwan Poudel <bkspoudel@gmail.com>
2025-11-19 22:20:29 +0100

    apply container to the newly created view:hooks

--- src/app/views/hooks/hooks.component.ts
+++ src/app/views/hooks/hooks.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '../../components/container/container.component';
 
 @Component({
   selector: 'app-hooks',
-  imports: [],
+  imports: [ContainerComponent],
   templateUrl: './hooks.component.html',
   styleUrl: './hooks.component.scss'
 })
