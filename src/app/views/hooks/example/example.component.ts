12705451386c61794ec2aaedee0af1bf589b3f6a
Bhuwan Poudel <bkspoudel@gmail.com>
2025-11-20 22:02:44 +0100

    implement all the lifecycle hooks in the child component

--- src/app/views/hooks/example/example.component.ts
+++ src/app/views/hooks/example/example.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-example',
@@ -6,7 +6,14 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
   templateUrl: './example.component.html',
   styleUrl: './example.component.scss'
 })
-export class ExampleComponent implements OnChanges {
+export class ExampleComponent implements OnChanges, 
+  OnInit, 
+  AfterContentInit, 
+  AfterContentChecked, 
+  AfterViewChecked,
+  AfterViewInit, 
+  DoCheck,
+  OnDestroy {
  title = "New Component";
  @Input()message: string;
 
@@ -14,4 +21,27 @@ export class ExampleComponent implements OnChanges {
    console.log("onChanges called...");
    console.log('Changes: ', changes);
  }
+
+ ngAfterContentChecked(): void {
+   console.log('AfterContentChecked');
+ }
+ ngAfterViewChecked(): void {
+   console.log('AfterViewChecked....')
+ }
+ ngDoCheck(): void {
+   console.log('DoCheck...')
+ }
+ ngOnInit(): void {
+   console.log('OnInit...')
+ }
+ ngAfterViewInit(): void {
+   console.log('AfterViewInit')
+ }
+ ngAfterContentInit(): void {
+   console.log('AfterContentInit...')
+ }
+ 
+ ngOnDestroy(): void {
+  console.log('Destroyed....')
+}
 }
--- src/app/views/hooks/hooks.component.ts
+++ src/app/views/hooks/hooks.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
 import { ExampleComponent } from './example/example.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-hooks',
-  imports: [ContainerComponent, ExampleComponent],
+  imports: [ContainerComponent, ExampleComponent, NgIf],
   templateUrl: './hooks.component.html',
   styleUrl: './hooks.component.scss'
 })
 export class HooksComponent {
   inputValue = '';
+  toBeDestroyed = true;
 
   onSubmit(el: HTMLInputElement){
     this.inputValue = el.value;
+  }
+
+  onDestroy(){
+    this.toBeDestroyed = !this.toBeDestroyed;
   }
 }
