70f4a82f4660df326a8072003bf370159899020e
Bhuwan Poudel <bkspoudel@gmail.com>
2025-11-24 22:26:55 +0100

    send dynamic data with the route and get it in the corresponding component

--- src/app/components/prototype/prototype.component.ts
+++ src/app/components/prototype/prototype.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Employee, Person } from './js-file';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-prototype',
@@ -11,11 +12,16 @@ export class Prototype1Component implements OnInit {
   person = new Person('sudhan', 43, 'lalim');
   employee = new Employee('Resham', 41, 'Kathmandu', '1122', 50000);
 
+  router: Router = inject(Router);
+
   ngOnInit(): void {
       console.log(this.person);
       console.log(this.employee);
 
       console.log(Employee.officeLocation());
+
+      // console.log(this.router.getCurrentNavigation().extras.state);
+      console.log('State:   ',history.state);
   }
 
 }
