03910ee56060c9c522e52d8de1ef0094e9aa5a59
Bhuwan Poudel <bkspoudel@gmail.com>
2025-12-08 17:45:57 +0100

    add date conversion function in js-practice

--- src/app/views/js-practive/js-practive.component.ts
+++ src/app/views/js-practive/js-practive.component.ts
@@ -10,4 +10,30 @@ import { ContainerComponent } from '../../components/container/container.compone
 })
 export class JsPractiveComponent {
 
+  dateConversion(dates: string[]){
+    let result = [];
+
+    for(let date of dates){
+        // ccc = date.split('-');
+        // console.log(date);
+        // console.log(ccc[ccc.length - 1]);
+
+        let convertedDate = date.replace(/\D/g, "");
+
+            let year = convertedDate.slice(0,4);
+            let month = convertedDate.slice(4,6);
+            let day = convertedDate.slice(6, 8);
+        // if(ccc[ccc.length - 1]!==4){
+        //      day = convertedDate.slice(0,2);
+        //      month = convertedDate.slice(2,4);
+        //      year = convertedDate.slice(4, 8);
+        // }
+
+        let res = `${year}/${month}/${day}`;
+        result.push(res);
+
+    console.log(result);
+}
+}
+
 }
