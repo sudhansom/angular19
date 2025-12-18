4b561cb5b4bcf289cd169a31a09c5da0655825bf
Bhuwan Poudel <bkspoudel@gmail.com>
2025-12-18 14:36:40 +0100

    apply condition in the directive

--- src/app/directives/if.directive.ts
+++ src/app/directives/if.directive.ts
@@ -8,7 +8,11 @@ export class IfDirective {
   constructor(private element: TemplateRef<any>, private view: ViewContainerRef) { }
 
   @Input() set display(value: boolean){
-    
+    if(value){
+      this.view.createEmbeddedView(this.element);
+    }else {
+      this.view.clear();
+    }
   }
 
 }
