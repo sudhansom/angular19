3c1f19ce643c93455ac0174f68cd9f01bb55313b
Bhuwan Poudel <bkspoudel@gmail.com>
2026-01-15 11:47:42 +0100

    update the class according to the entries given in the host element via set method in the directive class

--- src/app/directives/app-class.directive.ts
+++ src/app/directives/app-class.directive.ts
@@ -1,10 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appAppClass]'
+  selector: '[appClass]'
 })
 export class AppClassDirective {
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
+
+  @Input() set appClass(value: Object) {
+    let entries = Object.entries(value);
+    for(let entry of entries){
+      let [className, condition]= entry;
+      if(condition){
+        this.renderer.addClass(this.element.nativeElement, className);
+      }
+    }
+  }
 
 }
