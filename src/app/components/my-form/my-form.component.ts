78af00690eecb7ca6d78d0419acb31f66f9b3b14
Bhuwan Poudel <bkspoudel@gmail.com>
2026-01-24 14:52:49 +0100

    create a component to take inputs

---
+++ src/app/components/my-form/my-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-form',
+  imports: [],
+  templateUrl: './my-form.component.html',
+  styleUrl: './my-form.component.scss'
+})
+export class MyFormComponent {
+
+}
