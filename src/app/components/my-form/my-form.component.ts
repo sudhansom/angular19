b3dfbf240670cd82838cc30661e6d7f86240fb1c
Bhuwan Poudel <bkspoudel@gmail.com>
2026-01-25 22:30:59 +0100

    create a form inputs and import ngForm and formsmodule

--- src/app/components/my-form/my-form.component.ts
+++ src/app/components/my-form/my-form.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ContainerComponent } from '../container/container.component';
+import { NgForm, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-my-form',
-  imports: [ContainerComponent],
+  imports: [ContainerComponent, FormsModule],
   templateUrl: './my-form.component.html',
   styleUrl: './my-form.component.scss'
 })
 export class MyFormComponent {
 
+  @ViewChild('form') form: NgForm;
+  formSubmit(){
+    console.log(this.form.value);
+  }
 }
