4e9d27a35679aa8aeee78a67d2ee86028ed46053
Bhuwan Poudel <bkspoudel@gmail.com>
2026-01-30 13:35:24 +0100

    use Subject for the allUsers as an obsevable

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,25 +1,25 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { EachUserComponent } from '../each-user/each-user.component';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user-service.service';
 import { FormsModule } from '@angular/forms';
-import { NgFor } from '@angular/common';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
-  imports: [EachUserComponent, NgFor, FormsModule],
+  imports: [ NgFor, FormsModule, AsyncPipe],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent implements OnInit {
-  allUsers: User[] = [];
+
+  allUsers: Subject<User[]> = new Subject<User[]>();
 
   userService: UserService = inject(UserService);
 
   ngOnInit() {
     this.userService.getUsers().subscribe(users => {
-      this.allUsers = users;
-      console.log('Users from the User List: ',users)
+      this.allUsers.next(users);
     });
   }
 
