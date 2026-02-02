401f4ae0407434a71a78690a0eca160d5f2679d8
Bhuwan Poudel <bkspoudel@gmail.com>
2026-02-02 22:35:00 +0100

    delete the update functionality for now

--- src/app/components/my-form/my-form.component.ts
+++ src/app/components/my-form/my-form.component.ts
@@ -32,31 +32,19 @@ export class MyFormComponent implements OnInit, AfterViewInit {
     this.userService.getUsers().subscribe(users => {
       console.log('All Users: ', users);
     })
+
+    this.userService.selectedUser.subscribe(user => {
+      this.currentUser = user;
+    })
   }
 
   ngAfterViewInit(): void {
-    // this.form.valueChanges.pipe(
-    //   filter(() => this.form.valid),
-    //   debounceTime(10000),
-    //   concatMap(changes => this.updateForm(changes))
-    // ).subscribe(
-    //   value => {
-    //   // console.log(value);
-    //   // console.log('form is valid:',this.form.valid);
-
-    // })
+   
   }
   formSubmit(){
     // this.userService.saveData(this.form.value);
     this.userService.createUser(this.form.value).subscribe(user => {
       console.log(user);
     })
-  }
-  updateForm(changes){
-      if(this.currentUser){
-        return this.userService.updateUser('d23dea54-f211-4f06-9e41-b2366acdcede', changes);
-      }else {
-        return changes
-      }
   }
 }
